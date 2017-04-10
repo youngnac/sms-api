495420744f4e68727e5e6a84958102ddbcdaddd9
youngnac <youngnac@gmail.com>
2017-04-11 00:41:01 +0900

    간단히 sms api test를 위한 form based views 생성

--- django_app/sms/forms.py
+++ django_app/sms/forms.py
@@ -1,9 +1,26 @@
+import re
+
 from django import forms
 
 
 class MessageForm(forms.Form):
-    to_number = forms.IntegerField(label="수신번호 : '-', 공백 없이 넣어주세요")
+    to_number = forms.CharField(label="수신번호 : '-', 공백 없이 넣어주세요", widget=forms.TextInput(attrs={'size': '100'}))
     text = forms.CharField(max_length=500,
                            widget=forms.Textarea,
                            label="문자내용",
                            )
+
+    def clean_recipient_numbers(self):
+        cleaned_numbers = []
+        p = re.compile(r'^0\d{9}\d?$')
+
+        number_string = self.cleaned_data['to_number']
+        numbers = number_string.replace('-', '').replace(' ', '').split(',')
+        for number in numbers:
+            if re.match(p, number):
+                cleaned_numbers.append(number)
+                print(cleaned_numbers)
+            else:
+                pass
+                # raise ValidationError('Invalid phone number format! {}'.format(number))
+        return cleaned_numbers
--- django_app/sms/views.py
+++ django_app/sms/views.py
@@ -1,6 +1,5 @@
 import json
 import os
-import sys
 
 from django.shortcuts import render, redirect
 from sdk.api.message import Message
@@ -13,8 +12,10 @@ def text_send_view(request):
     if request.method == 'POST':
         form = forms.MessageForm(request.POST)
         if form.is_valid():
+
             message = request.POST["text"]
-            to_number = request.POST["to_number"]
+
+            to_number = form.cleaned_data['to_number']
 
             CONF_DIR = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))),
                                     '.conf')
@@ -22,27 +23,29 @@ def text_send_view(request):
             api_key = conf["sms"]["api_key"]
             api_secret = conf["sms"]["api_secret"]
             from_num = conf["sms"]["sender_number"]
+            print(to_number)
 
-            params = {
-                'type': 'sms',
-                'to': to_number,
-                'from': from_num,
-                'text': message,
-            }
-            cool = Message(api_key, api_secret)
+            for single_num in to_number:
+                params = {
+                    'type': 'sms',
+                    'to': single_num,
+                    'from': from_num,
+                    'text': message,
+                }
+                cool = Message(api_key, api_secret)
 
-            try:
-                response = cool.send(params)
-                print("Success Count : %s" % response['success_count'])
-                print("Error Count : %s" % response['error_count'])
-                print("Group ID : %s" % response['group_id'])
+                try:
+                    response = cool.send(params)
+                    print("Success Count : %s" % response['success_count'])
+                    print("Error Count : %s" % response['error_count'])
+                    print("Group ID : %s" % response['group_id'])
 
-                if "error_list" in response:
-                    print("Error List : %s" % response['error_list'])
+                    if "error_list" in response:
+                        print("Error List : %s" % response['error_list'])
 
-            except CoolsmsException as e:
-                print("Error Code : %s" % e.code)
-                print("Error Message : %s" % e.msg)
+                except CoolsmsException as e:
+                    print("Error Code : %s" % e.code)
+                    print("Error Message : %s" % e.msg)
             # sys.exit()
             return redirect("index")
 
